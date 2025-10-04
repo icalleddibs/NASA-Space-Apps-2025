19545813c83f95936ec85633feb19ddca69bb051
Diba Alam <diba.alam@mail.utoronto.ca>
2025-10-04 15:21:33 -0400

    tagger dark mode

--- src/routes/2Dmap/+page.svelte
+++ src/routes/2Dmap/+page.svelte
@@ -1,5 +1,5 @@
 <h1 class="text-4xl font-bold mb-2">Live Shark Tag Tracker</h1>
-<p class="text-lg text-gray-700">A 2D interactive map displaying active shark tags.</p>
+<p class="text-lg text-gray-400">A 2D interactive map displaying active shark tags.</p>
 
 <script lang="ts">
     import {
@@ -10,9 +10,6 @@
         GlobeControl,
         Popup
     } from 'svelte-maplibre-gl';
-
-
-    //Create Shark Markers and import data here
 
     let lnglat = $state({ lng: -30, lat: 40 });
     let lngLatLoc = $derived(`(${lnglat.lat.toFixed(3)}, ${lnglat.lng.toFixed(3)})`);
@@ -46,133 +43,136 @@
 
     let offset = $state(24);
     let offsets: maplibregl.Offset = $derived({
-    top: [0, offset],
-    bottom: [0, -offset],
-    left: [offset + 12, 0],
-    right: [-offset - 12, 0],
-    center: [0, 0],
-    'top-left': [offset, offset],
-    'top-right': [-offset, offset],
-    'bottom-left': [offset, -offset],
-    'bottom-right': [-offset, -offset]
-  });
-
+      top: [0, offset],
+      bottom: [0, -offset],
+      left: [offset + 12, 0],
+      right: [-offset - 12, 0],
+      center: [0, 0],
+      'top-left': [offset, offset],
+      'top-right': [-offset, offset],
+      'bottom-left': [offset, -offset],
+      'bottom-right': [-offset, -offset]
+    });
 </script>
 
 <MapLibre
     class="h-[55vh] min-h-[600px]"
-    style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+    style="https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
     zoom={1.5}
     center={{ lng: -80, lat: 40 }}
     maxPitch={85}
     attributionControl={false}
-
 >
     <NavigationControl />
     <ScaleControl />
     <GlobeControl />
 
+    <!-- Marker 1 -->
     <Marker bind:lnglat>
     {#snippet content()}
       <div class="text-center leading-none">
         <div class="text-3xl">🦈</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatName}</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatLoc}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatName}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatLoc}</div>
       </div>
     {/snippet}
-    <Popup class="w-50 text-black" bind:open={popupOpen} offset={offsets}>
+    <Popup class="w-50 text-white" bind:open={popupOpen} offset={offsets}>
       <span class="text-lg font-bold">{lngLatName}</span>
-        <br />
-        <span class="text-sm italic">{lngLatLoc}</span>
-        <br />
-        <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
+      <br />
+      <span class="text-sm italic">{lngLatLoc}</span>
+      <br />
+      <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
     </Popup>
     </Marker>
 
+    <!-- Marker 2 -->
     <Marker bind:lnglat={lnglat2}>
     {#snippet content()}
       <div class="text-center leading-none">
         <div class="text-3xl">🦈</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatName2}</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatLoc2}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatName2}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatLoc2}</div>
       </div>
     {/snippet}
-    <Popup class="w-50 text-black" bind:open={popupOpen2} offset={offsets}>
+    <Popup class="w-50 text-white" bind:open={popupOpen2} offset={offsets}>
       <span class="text-lg font-bold">{lngLatName2}</span>
-        <br />
-        <span class="text-sm italic">{lngLatLoc2}</span>
-        <br />
-        <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
+      <br />
+      <span class="text-sm italic">{lngLatLoc2}</span>
+      <br />
+      <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
     </Popup>
     </Marker>
 
+    <!-- Marker 3 -->
     <Marker bind:lnglat={lnglat3}>
     {#snippet content()}
       <div class="text-center leading-none">
         <div class="text-3xl">🦈</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatName3}</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatLoc3}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatName3}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatLoc3}</div>
       </div>
     {/snippet}
-    <Popup class="w-50 text-black" bind:open={popupOpen3} offset={offsets}>
+    <Popup class="w-50 text-white" bind:open={popupOpen3} offset={offsets}>
       <span class="text-lg font-bold">{lngLatName3}</span>
-        <br />
-        <span class="text-sm italic">{lngLatLoc3}</span>
-        <br />
-        <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
+      <br />
+      <span class="text-sm italic">{lngLatLoc3}</span>
+      <br />
+      <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
     </Popup>
     </Marker>
 
+    <!-- Marker 4 -->
     <Marker bind:lnglat={lnglat4}>
     {#snippet content()}
       <div class="text-center leading-none">
         <div class="text-3xl">🦈</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatName4}</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatLoc4}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatName4}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatLoc4}</div>
       </div>
     {/snippet}
-    <Popup class="w-50 text-black" bind:open={popupOpen4} offset={offsets}>
+    <Popup class="w-50 text-white" bind:open={popupOpen4} offset={offsets}>
       <span class="text-lg font-bold">{lngLatName4}</span>
-        <br />
-        <span class="text-sm italic">{lngLatLoc4}</span>
-        <br />
-        <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
+      <br />
+      <span class="text-sm italic">{lngLatLoc4}</span>
+      <br />
+      <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
     </Popup>
     </Marker>
 
+    <!-- Marker 5 -->
     <Marker bind:lnglat={lnglat5}>
     {#snippet content()}
       <div class="text-center leading-none">
         <div class="text-3xl">🦈</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatName5}</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatLoc5}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatName5}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatLoc5}</div>
       </div>
     {/snippet}
-    <Popup class="w-50 text-black" bind:open={popupOpen5} offset={offsets}>
+    <Popup class="w-50 text-white" bind:open={popupOpen5} offset={offsets}>
       <span class="text-lg font-bold">{lngLatName5}</span>
-        <br />
-        <span class="text-sm italic">{lngLatLoc5}</span>
-        <br />
-        <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
+      <br />
+      <span class="text-sm italic">{lngLatLoc5}</span>
+      <br />
+      <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
     </Popup>
     </Marker>
 
+    <!-- Marker 6 -->
     <Marker bind:lnglat={lnglat6}>
     {#snippet content()}
       <div class="text-center leading-none">
         <div class="text-3xl">🦈</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatName6}</div>
-        <div class="font-bold text-black drop-shadow-xs">{lngLatLoc6}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatName6}</div>
+        <div class="font-bold text-white drop-shadow-xs">{lngLatLoc6}</div>
       </div>
     {/snippet}
-    <Popup class="w-50 text-black" bind:open={popupOpen6} offset={offsets}>
+    <Popup class="w-50 text-white" bind:open={popupOpen6} offset={offsets}>
       <span class="text-lg font-bold">{lngLatName6}</span>
-        <br />
-        <span class="text-sm italic">{lngLatLoc6}</span>
-        <br />
-        <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
+      <br />
+      <span class="text-sm italic">{lngLatLoc6}</span>
+      <br />
+      <span class="text-sm">Last updated: {new Date().toLocaleString()}</span>
     </Popup>
     </Marker>
 
 </MapLibre>
-
